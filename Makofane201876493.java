158b627972a703c691afcb109a3d00c1b1109b51
Ralph <rmj.ralph@gmail.com>
2024-05-07 15:48:42 +0200

    RMJ final commit, adding Makofane201876493

---
+++ Makofane201876493.java
@@ -0,0 +1,11 @@
+public class Makofane20187493 {
+  public static void main(String[] args) {
+    int num1 = 6;
+    int num2 = 6;
+
+    int sum = num1 + num2;
+
+    System.out.println("Addition of " + num1 + " & " + num2 + " gives us " + sum);
+    }
+  }
+
