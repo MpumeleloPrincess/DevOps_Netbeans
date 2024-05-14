b87b1ef610c997959ff0465ce9af8a8d9196cc2b
Mpumi <mpumelelop497@gmail.com>
2024-05-14 14:56:13 +0200

    my commit on the file

---
+++ OneDrive/Documentos/NetBeansProjects/Shabangu220138656/src/main/java/com/mycompany/shabangu220138656/Shabangu220138656.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shabangu220138656;
+
+/**
+ *
+ * @author HP
+ */
+public class Shabangu220138656 {
+
+    public static void main(String[] args) {
+        System.out.println("This is my make up test project class");
+    }
+}
